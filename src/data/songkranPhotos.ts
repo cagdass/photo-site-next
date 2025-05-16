53c7e8bbb82b81de04208bd5b6e8a9e2dabc394a
cagdass <badass@alumni.bilkent.edu.tr>
2025-05-16 11:29:21 +0200

    feat: update photos

---
+++ src/data/songkranPhotos.ts
@@ -0,0 +1,26 @@
+import type { Photo } from "react-photo-album";
+
+const photos: Photo[] = [
+  { src: "/images/songkran/songkran-01.jpg", width: 1198, height: 800, alt: "Songkran photo 01" },
+  { src: "/images/songkran/songkran-02.jpg", width: 1198, height: 800, alt: "Songkran photo 02" },
+  { src: "/images/songkran/songkran-03.jpg", width: 1198, height: 800, alt: "Songkran photo 03" },
+  { src: "/images/songkran/songkran-04.jpg", width: 1198, height: 800, alt: "Songkran photo 04" },
+  { src: "/images/songkran/songkran-05.jpg", width: 1198, height: 800, alt: "Songkran photo 05" },
+  { src: "/images/songkran/songkran-06.jpg", width: 1198, height: 800, alt: "Songkran photo 06" },
+  { src: "/images/songkran/songkran-07.jpg", width: 1198, height: 800, alt: "Songkran photo 07" },
+  { src: "/images/songkran/songkran-08.jpg", width: 1198, height: 800, alt: "Songkran photo 08" },
+  { src: "/images/songkran/songkran-09.jpg", width: 1198, height: 800, alt: "Songkran photo 09" },
+  { src: "/images/songkran/songkran-10.jpg", width: 1198, height: 800, alt: "Songkran photo 10" },
+  { src: "/images/songkran/songkran-11.jpg", width: 1198, height: 800, alt: "Songkran photo 11" },
+  { src: "/images/songkran/songkran-12.jpg", width: 1198, height: 800, alt: "Songkran photo 12" },
+  { src: "/images/songkran/songkran-13.jpg", width: 1198, height: 800, alt: "Songkran photo 13" },
+  { src: "/images/songkran/songkran-14.jpg", width: 1198, height: 800, alt: "Songkran photo 14" },
+  { src: "/images/songkran/songkran-15.jpg", width: 1198, height: 800, alt: "Songkran photo 15" },
+  { src: "/images/songkran/songkran-16.jpg", width: 1198, height: 800, alt: "Songkran photo 16" },
+  { src: "/images/songkran/songkran-17.jpg", width: 1198, height: 800, alt: "Songkran photo 17" },
+  { src: "/images/songkran/songkran-18.jpg", width: 1198, height: 800, alt: "Songkran photo 18" },
+  { src: "/images/songkran/songkran-19.jpg", width: 1198, height: 800, alt: "Songkran photo 19" },
+  { src: "/images/songkran/songkran-20.jpg", width: 1198, height: 800, alt: "Songkran photo 20" },
+];
+
+export default photos;
