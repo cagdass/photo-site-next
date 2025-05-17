bef37c833ae25ddbbc7100e0d2ffbee2c5046a8d
cagdass <badass@alumni.bilkent.edu.tr>
2025-05-17 02:29:15 +0200

    feat: write more on Carmen page

--- src/data/carmenEssayBlocks.tsx
+++ src/data/carmenEssayBlocks.tsx
@@ -7,7 +7,9 @@ import PhotoEssayText from '@/components/PhotoEssayText';
 const carmenEssayBlocks = [
   {
     type: 'text',
-    content: `One of the cheapest bars in Södermalm. No music, no waiters — just chatter. Which, for me, was ideal. I’ve always hated shouting over basslines. My Welsh friend used to say it was the only place in Stockholm that resembled the pubs back home. Small tables, packed close. A little dark. A little intimate.`,
+    content: `I first grabbed a beer in Carmen in 2021. I had cycled past it many times on my way home from work — always packed, always loud — but never went in.
+
+    It quickly became my go-to. One of the cheapest bars in Södermalm. No music, no posh waiters — just chatter. Which, for me, was ideal. I’ve always hated shouting over loud basslines, or splashing on organic craft beer. My Welsh friend used to say it was the only place in Stockholm that resembled the pubs back home. Small tables, packed close. A little dark. A little intimate.`,
   },
   {
     type: 'image',
@@ -16,7 +18,7 @@ const carmenEssayBlocks = [
   },
   {
     type: 'text',
-    content: `It was just down the street from my tiny bedsit — and for a while, it became a second home. When I was learning photography, I had a homework assignment: portraits of strangers under artificial light. Carmen instantly came to mind. I started bringing my camera often. Took portraits of friends, regulars, drunks, and whoever else sat still long enough.`,
+    content: `When I later moved into the city, my bedsit ended up just down the street from Carmen. For a while, it became a second home. When I was learning photography in Summer 2023, I had a homework assignment: portraits of strangers under artificial light. Carmen instantly came to mind. I started bringing my camera often. Took portraits of friends, regulars, drunks, and whoever else sat still long enough.`,
   },
   {
     type: 'image',
@@ -50,8 +52,13 @@ const carmenEssayBlocks = [
     caption: 'Before the flash — joy in layers',
   },
   {
-    type: 'text',
-    content: `Eventually, I did. The owner’s son — notorious among both my friends and the Google Maps reviews — pulled an ice cube prank on me one night, dropping it down the back of my shirt. One of those guys who acts like you’re friends, but you’re not. That was it. I stopped showing up. Stopped photographing there too.`,
+    type: 'component',
+    render:
+      () => (
+        <PhotoEssayText>
+          I made it a habit of going in just to grab a beer after work anyways. I would also talk to new faces, snap some photos, and head home. I was happy with having a long-term project where I would collect photos, I saw it as my own little <em>Café Lehmitz</em> — a long-term project in a single bar. Funny enough, Anders Petersen doesn’t live too far from Carmen either.
+        </PhotoEssayText>
+      )
   },
   {
     type: 'image',
@@ -65,7 +72,7 @@ const carmenEssayBlocks = [
   },
   {
     type: 'text',
-    content: `Carmen closed a few months later. It reopened later under new ownership. I haven’t gone back. But the practice I got there — the light, the faces, the stories — stayed with me.`,
+    content: `Eventually, I did. The owner’s son — notorious among both my friends and the Google Maps reviews — pulled an ice cube prank on me one night, dropping it down the back of my shirt. One of those guys who acts like you’re friends, but you’re not. That was it. I stopped showing up. Stopped photographing there too.`,
   },
   {
     type: 'image',
@@ -82,6 +89,10 @@ const carmenEssayBlocks = [
     src: '/images/carmen/carmen-28.jpg',
     caption: 'Rain, neon, and memory outside Carmen',
     orientation: 'portrait',
+  },
+  {
+    type: 'text',
+    content: `My friends were right. Carmen closed a few months after I stopped going. It is still a running joke between us. It did reopen many months later under new ownership. I haven’t gone back. Life has taken me to many other places since, but the practice I got there — the light, the faces, the stories — stayed with me.`,
   },
   {
     type: 'image',
