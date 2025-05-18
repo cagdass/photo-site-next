2b2d26135b847c982c6d3aef4136cf5a77071314
cagdass <badass@alumni.bilkent.edu.tr>
2025-05-18 22:22:37 +0200

    fix: add redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/projects/soi6",
+        destination: "/soi6",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
